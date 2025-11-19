48dcdfa1912c41d31f080f026ed51ac46b3362da
Patryk Kepa <patryk.kepa@payarto.com>
2025-11-19 14:45:02 +0100

    vercel setup

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    base: '/',
+
     optimizeDeps: {
         noDiscovery: true
     },
